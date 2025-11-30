fd8a02d85297dc0f3f5d76a8446af84847777ee3
sabeer-ivl <sabeer.waqas@indusvalleylabs.com>
2025-11-30 22:08:58 +0500

    chore: learn classes and constructor

---
+++ classes/ClassesObjDemo.java
@@ -0,0 +1,11 @@
+public class ClassesObjDemo {
+    public static void main(String[] args){
+        ClassesDemo car1 = new ClassesDemo();
+        car1.brand = "Toyota";
+        car1.color = "White";
+        car1.speed = 100;
+
+        car1.drive();
+
+    }
+}
